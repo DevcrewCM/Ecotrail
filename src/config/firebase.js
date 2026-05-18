348baf27b7ac08d9cf35c8d9f92e6a0f9580739e
Cristian Meneses <meneses.185189@e.unavarra.es>
2026-05-19 01:02:39 +0200

    chore: Mover credenciales de Firebase a variables de entorno (.env)

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCu_a0M4dlCcfPXYjYhvFunPNzygNFR_nQ",
-  authDomain: "ecotrail-4c70d.firebaseapp.com",
-  projectId: "ecotrail-4c70d",
-  storageBucket: "ecotrail-4c70d.firebasestorage.app",
-  messagingSenderId: "1095165935357",
-  appId: "1:1095165935357:web:3486954a59c807ffc44894",
-  measurementId: "G-ZLX5Z7J73S"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
